bb751f39c5f26cee01cf8c868498018172fe3cbc
KayongoYongo <sknsammie10@gmail.com>
2024-04-24 23:22:00 +0300

    Added the quotes API

--- quotes.py
+++ quotes.py
@@ -1,25 +1,50 @@
+import os
+from dotenv import load_dotenv
 import requests
 
-def get_quotes(mode):
+# load environmental variables
+load_dotenv()
+
+def get_quotes(mode_or_category):
     """
     Description:
-        This function interacts with the api to retrieve information
-        about a quote
+        This function interacts with the appropriate API to retrieve information
+        about a quote based on the mode or category provided.
 
     Args:
-        mode: This can be random, today or image
-    
+        mode_or_category: This can be 'random', 'today', 'image', or a category for the quote.
+
     Return:
-        A JSON response
+        A JSON response or an image
     """
-    # Base url
-    url = f"https://zenquotes.io/api/{mode}"
+    if mode_or_category in ['random','today','image']:
+        # Base url for zenquotes.io API
+        url = f"https://zenquotes.io/api/{mode_or_category}"
 
-    # The response
-    response = requests.get(url)
-    # print(dir(response))
+        # The response
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        return response.json()
+        # Check if the response is successful
+        if response.status_code == 200:
+            # Handle a separate instance of an image argument
+            if mode_or_category == 'image':
+                return response.content
+            else:
+                return response.json()
+        else:
+            return f"There is an error in the request to zenquotes.io"
     else:
-        return f"There is an error in the request"
+        # The api key for 
+        API_KEY = os.getenv('API_KEY')
+
+        # Base Url for api-ninjas.com API
+        url = f'https://api.api-ninjas.com/v1/quotes?category={mode_or_category}'
+
+        # The response
+        response = requests.get(url, headers={'X-Api-Key': API_KEY})
+
+        # Check if the response is successful
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"There is an error in the request to api-ninjas.com"
